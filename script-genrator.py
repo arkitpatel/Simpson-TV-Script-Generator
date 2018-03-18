74228c1cfa2568e36f529240875ddebfccab2763
Arkit Patel <arkitpatel@users.noreply.github.com>
2018-03-18 18:09:13 +0530

    Update script-genrator.py

---
+++ script-genrator.py
@@ -0,0 +1,87 @@
+import tensorflow as tf
+import numpy as np
+import helper
+import problem_unittests as tests
+
+_, vocab_to_int, int_to_vocab, token_dict = helper.load_preprocess()
+seq_length, load_dir = helper.load_params()
+
+def get_tensors(loaded_graph):
+    """
+    Get input, initial state, final state, and probabilities tensor from <loaded_graph>
+    :param loaded_graph: TensorFlow graph loaded from file
+    :return: Tuple (InputTensor, InitialStateTensor, FinalStateTensor, ProbsTensor)
+    """
+    # TODO: Implement Function
+    InputTensor = loaded_graph.get_tensor_by_name("input:0")
+    InitialStateTensor = loaded_graph.get_tensor_by_name("initial_state:0")
+    FinalStateTensor = loaded_graph.get_tensor_by_name("final_state:0")
+    ProbsTensor = loaded_graph.get_tensor_by_name("probs:0")
+    return InputTensor, InitialStateTensor, FinalStateTensor, ProbsTensor
+
+def pick_word(probabilities, int_to_vocab):
+    """
+    Pick the next word in the generated text
+    :param probabilities: Probabilites of the next word
+    :param int_to_vocab: Dictionary of word ids as the keys and words as the values
+    :return: String of the predicted word
+    """
+    # TODO: Implement Function
+    probability = max(probabilities)
+    index = probabilities.tolist().index(probability)
+    predicted_word = int_to_vocab[index]
+    return predicted_word
+
+gen_length = input("Set script generation length : ")
+options =  ['homer_simpson', 'moe_szyslak' ,  'Barney_Gumble']
+print("Choose Script for")
+print("1) Homer Simpson")
+print("2) Moe Szyslan")
+print("3) Barney Gumble")
+print("Enter 1, 2 or 3")
+index = int(input())
+if 0 < index < 4:
+    prime_word = options[index-1]
+else:
+    exit(0)
+
+"""
+DON'T MODIFY ANYTHING IN THIS CELL THAT IS BELOW THIS LINE
+"""
+loaded_graph = tf.Graph()
+with tf.Session(graph=loaded_graph) as sess:
+    # Load saved model
+    loader = tf.train.import_meta_graph(load_dir + '.meta')
+    loader.restore(sess, load_dir)
+
+    # Get Tensors from loaded model
+    input_text, initial_state, final_state, probs = get_tensors(loaded_graph)
+
+    # Sentences generation setup
+    gen_sentences = [prime_word + ':']
+    prev_state = sess.run(initial_state, {input_text: np.array([[1]])})
+
+    # Generate sentences
+    for n in range(gen_length):
+        # Dynamic Input
+        dyn_input = [[vocab_to_int[word] for word in gen_sentences[-seq_length:]]]
+        dyn_seq_length = len(dyn_input[0])
+
+        # Get Prediction
+        probabilities, prev_state = sess.run(
+            [probs, final_state],
+            {input_text: dyn_input, initial_state: prev_state})
+        
+        pred_word = pick_word(probabilities[dyn_seq_length-1], int_to_vocab)
+
+        gen_sentences.append(pred_word)
+    
+    # Remove tokens
+    tv_script = ' '.join(gen_sentences)
+    for key, token in token_dict.items():
+        ending = ' ' if key in ['\n', '(', '"'] else ''
+        tv_script = tv_script.replace(' ' + token.lower(), key)
+    tv_script = tv_script.replace('\n ', '\n')
+    tv_script = tv_script.replace('( ', '(')
+        
+    print(tv_script)
